bd36bc6b37bdf2358dacc96564ac91d5991408d0
unknown <you@example.com>
2025-11-07 05:05:55 +0530

    New commit

--- DoublyCircularLL/src/com/demo/service/DoublyCircularLL.java
+++ DoublyCircularLL/src/com/demo/service/DoublyCircularLL.java
@@ -1,5 +1,69 @@
 package com.demo.service;
 
 public class DoublyCircularLL {
+	Node head;
+	class Node
+	{
+		int data;
+		Node next;
+		Node prev;
+		
+	public Node(int data)
+	{
+		this.data=data;
+		next=null;
+		prev=null;
+				
+		}
+		
+	}
+	public DoublyCircularLL()
+	{
+		head=null;
+	}
+	public void addNode(int val)
+	{
+		Node newNode= new Node(val);
+		
+		if(head==null)
+		{
+			head=newNode;
+			head.prev=head;
+			head.next=head;
+		}else
+		{
+			Node temp=head.prev;
+			temp.next=head;
+			newNode.next=head;
+			newNode.prev=temp;
+			temp.next=newNode;
+			head.prev=newNode;
+		}
+		
+		
+		
+		
+		
+	}
+	public void DisplayData()
+	{
+		Node temp=head;
+		if(head==null)
+		{
+			System.out.println("The list is null");
+		}
+		else
+		{
+			do
+			{
+				System.out.print(temp.data+ "---->");
+				temp=temp.next;
+			}
+			while
+				(temp!=head);
+			System.out.println();
+		}
+		
+	}
 
 }
--- DoublyCircularLL/src/com/demo/test/TestDoublyCircularLinkedList.java
+++ DoublyCircularLL/src/com/demo/test/TestDoublyCircularLinkedList.java
@@ -1,9 +1,24 @@
 package com.demo.test;
+import com.demo.service.DoublyCircularLL;
 
 public class TestDoublyCircularLinkedList {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		DoublyCircularLL L=new DoublyCircularLL();
+		L.addNode(10100);
+		L.addNode(12000);
+		L.addNode(13000);
+		L.addNode(14000);
+		L.addNode(15000);
+		L.addNode(106000);
+		L.addNode(1005400);
+		L.addNode(1000540);
+		L.addNode(100040);
+		L.addNode(1000540);
+		L.addNode(1000460);		
+		L.DisplayData();
+		
+		
 
 	}
 
